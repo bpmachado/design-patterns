d05fac556561e37ee94e2971305287a63398328e
bpmachado <bpmachado@gmail.com>
2025-09-23 14:57:12 -0300

    Padrao stratagy

---
+++ strategy/src/main/java/br/com/bpmachado1986/strategy/strategy/StrategyApplication.java
@@ -0,0 +1,13 @@
+package br.com.bpmachado1986.strategy.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyApplication.class, args);
+	}
+
+}
---
+++ strategy/src/test/java/br/com/bpmachado1986/strategy/strategy/StrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bpmachado1986.strategy.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
